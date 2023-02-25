5dbc2268f841f639d4bbfa1ce19ef568eb3417cf
Nidal Mohammed <nidalmo23@gmail.com>
2023-02-25 19:32:31 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: 'burtons',
+      }
+    },
+  },
+  plugins: [],
+}
